06df37f7f7e72adc3b04956605223627d87c7aba
Clyde Charles <clydecc09@gmail.com>
2025-05-18 10:21:05 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,13 +8,13 @@ import { DiCss3, DiJavascript } from "react-icons/di";
 import { SiMysql } from "react-icons/si";
 import { FaPhp } from "react-icons/fa";
 import { FaBootstrap } from "react-icons/fa";
-import vrsm from '../components/ProjectsImages/vrsm.png'
+import {vrsm} from '../components/ProjectsImages/vrsm.png'
 
 const projects = [
   {
     title: 'Vehicle Rental System',
     description: 'The Vehicle Rental System is a web-based application designed to simplify the process of renting vehicles. Built using HTML, CSS, and JavaScript for a dynamic and user-friendly front-end interface, the system is powered by PHP and MySQL for robust back-end operations and data management.',
-    image: 'https://www.fleetroot.com/wp-content/uploads/2020/08/20-features-in-car-rental-software-in-2020-1.jpg',
+    image: 'vrsm',
     technologies: ['html', 'css', 'js', 'bootstrap','MySql', 'php'],
     liveUrl: 'https://github.com/colloid09/vehicle-rental-system'
   },
